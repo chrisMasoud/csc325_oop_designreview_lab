8947ea4e596484c54c9f6b90bc6f9a7f31a6ca13
chrisMasoud <chrismasoud22@gmail.com>
2023-02-28 13:53:19 -0500

    Todo #4: Added comments to all my code

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -1,37 +1,58 @@
 package com.mycompany.csc325_oop_designreview_lab;
 
 /**
- *
- * @author MoaathAlrajab
+ * The Student class extends from Human and holds GPA
+ * @author Chris Masoud
  */
 /**
  *
  * @author MoaathAlrajab
  */
 public class Student extends Human {
+    //double to hold GPA
     private double gpa;
 
+    /**
+     * The Student constructor takes in a name and age and creates an instance 
+     * of Student
+     * @param name
+     * @param age 
+     */
     public Student(String name, short age) {
         super(name, age);
     }
 
+    /**
+     * The getAddress() method returns the Student's address
+     * @return String address
+     */
     @Override
     public String getAddress() {
         return this.getAddress();
     }
 
+    /**
+     * The setAddress() method takes in an address and assigns it to the
+     * Student's address
+     * @param address 
+     */
     @Override
     public void setAddress(String address) {
         this.setAddress(address);
     }
 
-    // ToDo 3: Add a field for GPA and create setter and getter
-    // ToDo 4: Add comments to your code
-
+    /**
+     * The getGpa() method returns the Student's GPA
+     * @return double gpa
+     */
     public double getGpa() {
         return gpa;
     }
 
+    /**
+     * The setGpa() method takes in a double and assigns it to the Student's GPA
+     * @param gpa 
+     */
     public void setGpa(double gpa) {
         this.gpa = gpa;
     }
